391d51d17c23776a2164dde36e3ebf3ccc6d8093
3A732087 <quannn0180716v@gmail.com>
2022-03-19 15:50:48 +0800

    Services建立(Create, Read, Delete)、Model建立

---
+++ MVCToDoList_Api/Models/ToDoList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCToDoList_Api.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ MVCToDoList_Api/Services/ToDoListService.cs
@@ -0,0 +1,133 @@
+﻿using MVCToDoList_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVCToDoList_Api.Services
+{
+    public class ToDoListService
+    {
+        //連線字串
+        private readonly static string cnstr = ConfigurationManager.ConnectionStrings["ToDoListApi"].ConnectionString;
+
+        //資料庫連線
+        private readonly SqlConnection conn = new SqlConnection(cnstr);
+
+        #region 查詢所有待辦事項
+        public List<ToDoList> GetAllToDoList()
+        {
+            List<ToDoList> DataList = new List<ToDoList>();
+            string sql = @"select m.* from ToDoList m";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ToDoList Data = new ToDoList();
+                    Data.Id = Convert.ToInt32(dr["Id"]);
+                    Data.Title = dr["Title"].ToString();
+                    Data.Level = dr["Level"].ToString();
+                    Data.Deadline = Convert.ToDateTime(dr["Deadline"]);
+
+                    DataList.Add(Data);
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return DataList;
+
+        }
+        #endregion
+
+        #region 查詢一筆待辦事項
+        public ToDoList GetToDoListById(int Id)
+        {
+            ToDoList Data = new ToDoList();
+
+            string sql = $@"select m.* from ToDoList m where 1=1 and m.Id = {Id}";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                Data.Id = Convert.ToInt32(dr["Id"]);
+                Data.Title = dr["Title"].ToString();
+                Data.Level = dr["Level"].ToString();
+                Data.Deadline = Convert.ToDateTime(dr["Deadline"]);
+            }
+            catch(Exception e)
+            {
+                Data = null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return Data;
+
+        }
+        #endregion
+
+        #region 新增待辦事項
+        public void InsertToDoList(ToDoList newData)
+        {
+            string sql = $@"insert into ToDoList (Title, Level, Dealline) value ('{newData.Title}','{newData.Level}','{DateTime.Now.ToString("yyyy/MM/dd")}')";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        #endregion
+
+        #region 刪除待辦事項
+        public void UpdateToDoList(int Id)
+        {
+            string sql = $@"delete from ToDoList where 1=1 and Id = {Id}";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        #endregion
+    }
+}
